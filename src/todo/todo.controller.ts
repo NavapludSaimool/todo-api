bdec2bcd6e32bd39ecb262697921c64907420229
NavapludSaimool <82089985+NavapludSaimool@users.noreply.github.com>
2024-09-06 11:43:47 +0700

    1st ver

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('todos')
+@Controller('todos')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+  create(@Body() createTodoDto: CreateTodoDto): Todo {
+    return this.todoService.create(createTodoDto);
+  }
+
+  @Get()
+  findAll(): Todo[] {
+    return this.todoService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Todo {
+    return this.todoService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto): Todo {
+    return this.todoService.update(+id, updateTodoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): void {
+    return this.todoService.remove(+id);
+  }
+}
---
+++ src/todo/todo.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
+
+@Injectable()
+export class TodoService {
+  private todos: Todo[] = [];
+  private idCounter = 1;
+
+
+  create(todoDto: CreateTodoDto): Todo {
+    const todo: Todo = {
+      id: this.idCounter++,
+      ...todoDto,
+    };
+    this.todos.push(todo);
+    return todo;
+  }
+
+  findAll() {
+    return this.todos;
+  }
+
+  findOne(id: number) {
+    return this.todos.find(todo => todo.id === id);
+  }
+
+  update(id: number, updateTodoDto: UpdateTodoDto): Todo {
+    const todo = this.findOne(id);
+    if (todo) {
+      Object.assign(todo, updateTodoDto);
+    }
+    return todo;
+  }
+
+  remove(id: number): void {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+}
