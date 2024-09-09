2e3088b7ad079bff4dd64c5948bc52e4c7b84b4f
NavapludSaimool <82089985+NavapludSaimool@users.noreply.github.com>
2024-09-09 12:04:33 +0700

    update database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { DatabaseModule } from './database/database.module';
-import { DatabaseService } from './database/database.service';
 
 @Module({
-  imports: [TodoModule, DatabaseModule, ConfigModule.forRoot()],
+  imports: [TodoModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService, DatabaseService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,18 @@
-import { Injectable, Inject } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  constructor(@Inject('POSTGRES_POOL') private readonly sql: any) {}
-  async getTable(name: string): Promise<any[]> {
-    return await this.sql(`SELECT * FROM ${name}`);
-  }
-}
-
-// import { Injectable } from '@nestjs/common';
+// import { Injectable, Inject } from '@nestjs/common';
 
 // @Injectable()
 // export class AppService {
-//   getHello(): string {
-//     return 'Hello World!';
+//   constructor(@Inject('POSTGRES_POOL') private readonly sql: any) {}
+//   async getTable(name: string): Promise<any[]> {
+//     return await this.sql(`SELECT * FROM ${name}`);
 //   }
 // }
+
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
